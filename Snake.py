633c4fe650be643c317cf58dd6d64f68ce45f4b0
Kevin Daniel <Dsanchez0397@gmail.com>
2021-03-13 11:50:10 -0600

    4 serpientes

--- Snake.py
+++ Snake.py
@@ -1,9 +1,22 @@
 from pygame import Rect
 from pygame.math import Vector2
 
+RED = (183, 70, 70)
+GREEN = (126, 183, 69)
+CYAN = (69, 183, 183)
+VIOLET = (126, 69, 183)
+
+players_body = [[Vector2(3,   3), Vector2(4 ,  3), Vector2(5 , 3)],
+                [Vector2(17,  3), Vector2(17,  4), Vector2(17, 5)],
+                [Vector2(17, 18), Vector2(16, 18), Vector2(15, 18)],
+                [Vector2(3,  18), Vector2(3 , 17), Vector2(3 , 16)]]
+
+players_color = [RED, GREEN, CYAN, VIOLET] 
+
 class Snake: 
-    def __init__(self, cell_size):
-        self.body = [Vector2(5, 10), Vector2(5, 11), Vector2(5, 12)]
+    def __init__(self, cell_size, player):
+        self.body = players_body[player]
+        self.color = players_color[player]
         self.cell_size = cell_size
     
     def getSnake(self):
--- game.py
+++ game.py
@@ -3,27 +3,32 @@ from pygame.locals import *
 from Fruit import Fruit
 from Snake import Snake
 
+def draw_fruit(fruit):
+    pygame.draw.ellipse(screen, RED, fruit.draw_fruit())
+
+def draw_snake(snake):
+    pos = snake.getSnake()
+    for block in pos:
+        pygame.draw.rect(screen, snake.color, block)
+
+GRAY = (98, 155, 98)
 GREEN = (175, 215, 70)
 BLUE = (70, 70, 190)
-RED = (183, 70, 70)
+RED = (255, 30, 15)
 
 cell_size = 30
 cell_number = 20
 screen_size = cell_number*cell_size
 
 fruit = Fruit(cell_size, cell_number)
-snake = Snake(cell_size)
+snakes = []
+for i in range(4):
+    snake = Snake(cell_size, i)
+    snakes.append(snake)
 
 pygame.init()
 screen = pygame.display.set_mode((screen_size, screen_size))
 clock = pygame.time.Clock()
-
-screen.fill(GREEN)
-pygame.draw.rect(screen, BLUE, fruit.draw_fruit())
-pos = snake.getSnake()
-for block in pos:
-    print(block)
-    pygame.draw.rect(screen, RED, block)
 
 while True:
     for event in pygame.event.get():
@@ -32,4 +37,8 @@ while True:
             sys.exit()
     
     pygame.display.update()
+    screen.fill(GRAY)
+    draw_fruit(fruit)
+    for snake in snakes:
+        draw_snake(snake)
     clock.tick(60)
