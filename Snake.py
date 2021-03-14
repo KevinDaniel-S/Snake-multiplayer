c2955796a35b4c48b6bf82aff221d6dbe6b3c143
Kevin Daniel <Dsanchez0397@gmail.com>
2021-03-14 16:02:05 -0600

    Moving snakes

--- Snake.py
+++ Snake.py
@@ -6,10 +6,10 @@ GREEN = (126, 183, 69)
 CYAN = (69, 183, 183)
 VIOLET = (126, 69, 183)
 
-players_body = [[Vector2(3,   3), Vector2(4 ,  3), Vector2(5 , 3)],
-                [Vector2(17,  3), Vector2(17,  4), Vector2(17, 5)],
-                [Vector2(17, 18), Vector2(16, 18), Vector2(15, 18)],
-                [Vector2(3,  18), Vector2(3 , 17), Vector2(3 , 16)]]
+players_body = [[Vector2(5,   3), Vector2(4 ,  3), Vector2(3 , 3)],
+                [Vector2(17,  5), Vector2(17,  4), Vector2(17, 3)],
+                [Vector2(15, 17), Vector2(16, 17), Vector2(17, 17)],
+                [Vector2(3,  15), Vector2(3 , 16), Vector2(3 , 17)]]
 
 players_color = [RED, GREEN, CYAN, VIOLET] 
 
@@ -18,12 +18,19 @@ class Snake:
         self.body = players_body[player]
         self.color = players_color[player]
         self.cell_size = cell_size
+        self.is_alive = True
+        self.direction = Vector2(0, 1)
     
-    def getSnake(self):
+    def get_snake(self):
         fruit_rects = []
         for block in self.body:
-            fruit_rect = Rect(int(block.x * self.cell_size), 
-                int(block.y * self.cell_size),
-                self.cell_size, self.cell_size)
+            x = int(block.x * self.cell_size)
+            y = int(block.y * self.cell_size)
+            fruit_rect = Rect(x, y, self.cell_size, self.cell_size)
             fruit_rects.append(fruit_rect)
         return fruit_rects
+
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+from pygame.math import Vector2
 from Fruit import Fruit
 from Snake import Snake
 
@@ -7,9 +8,27 @@ def draw_fruit(fruit):
     pygame.draw.ellipse(screen, RED, fruit.draw_fruit())
 
 def draw_snake(snake):
-    pos = snake.getSnake()
+    pos = snake.get_snake()
     for block in pos:
         pygame.draw.rect(screen, snake.color, block)
+
+class Main:
+    def __init__(self, players:"int 1:4"):
+        self.snakes = []
+        self.fruit = Fruit(cell_size, cell_number)
+        for i in range(players):
+            snake = Snake(cell_size, i)
+            self.snakes.append(snake)
+
+    def update(self):
+        for snake in self.snakes:
+            snake.move_snake()
+
+    def draw_elements(self):
+        draw_fruit(self.fruit)
+        for snake in self.snakes:
+            draw_snake(snake)
+
 
 GRAY = (98, 155, 98)
 GREEN = (175, 215, 70)
@@ -20,25 +39,39 @@ cell_size = 30
 cell_number = 20
 screen_size = cell_number*cell_size
 
-fruit = Fruit(cell_size, cell_number)
-snakes = []
-for i in range(4):
-    snake = Snake(cell_size, i)
-    snakes.append(snake)
-
 pygame.init()
 screen = pygame.display.set_mode((screen_size, screen_size))
 clock = pygame.time.Clock()
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+
+main_game = Main(4)
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            main_game.update()
+
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                for snake in main_game.snakes:
+                    snake.direction = Vector2(0, -1)
+            if event.key == K_DOWN:
+                for snake in main_game.snakes:
+                    snake.direction = Vector2(0, 1)
+            if event.key == K_LEFT:
+                for snake in main_game.snakes:
+                    snake.direction = Vector2(-1, 0)
+            if event.key == K_RIGHT:
+                for snake in main_game.snakes:
+                    snake.direction = Vector2(1, 0)
+       
     
-    pygame.display.update()
     screen.fill(GRAY)
-    draw_fruit(fruit)
-    for snake in snakes:
-        draw_snake(snake)
+    main_game.draw_elements()
+    pygame.display.update()
     clock.tick(60)
